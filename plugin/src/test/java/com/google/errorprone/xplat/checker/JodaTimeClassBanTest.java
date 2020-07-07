e854393a402398f3853aa15f8a3fb111cc401bff
David Horowitz <dhwitz@google.com>
2020-07-07 19:08:23 -0400

    Fixed link in test documentation in several files and updated LegacyTimeBan to use built in annotation suppression (#12)

--- plugin/src/test/java/com/google/errorprone/xplat/checker/JodaTimeClassBanTest.java
+++ plugin/src/test/java/com/google/errorprone/xplat/checker/JodaTimeClassBanTest.java
@@ -22,7 +22,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- * Unit tests for {@link MyCustomCheck}.
+ * Unit tests for {@link JodaTimeClassBan}.
  */
 @RunWith(JUnit4.class)
 public class JodaTimeClassBanTest {
